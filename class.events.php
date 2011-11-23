81e873bf53bd71ba1d598a87b0f218bdaa56ff8c
TheRealBenSmith <ben.adam.smith@gmail.com>
2011-11-22 21:58:05 -0500

    Initial commit of Marketing Events API Client

---
+++ class.events.php
@@ -0,0 +1,73 @@
+<?php
+require_once('class.baseclient.php');
+
+class Events extends BaseClient {
+    //Client for HubSpot Marketing Events API.
+
+    //Define required client variables
+    protected $API_PATH = 'events';    
+    protected $API_VERSION = 'v1';
+    
+    /**
+    * Get a list of events
+    *
+    * @returns Array of Marketing Events as stdObjects
+    *
+    * @throws exception
+    **/
+    public function get_events() {
+        $endpoint = 'events';
+        try {
+            return json_decode($this->execute_get_request($this->get_request_url($endpoint,null))); 
+        } catch (Exception $e) {
+            throw new Exception('Unable to retrieve settings: ' . $e);
+        }
+    }
+
+    /**
+    * Add a Marketing Event
+    *
+    * @param description: String description up to 150 chars 
+    * @param createDate: String ms since epoch (optional)
+    * @param eventURL: String URL for event up to 255 chars (optional)
+    * @param eventType: String short tag describing event (extremely optional)
+    *
+    * @returns Body of POST request
+    *
+    * @throws exception
+    **/
+    public function add_event($description, $createDate, $eventURL, $eventType) {
+        $endpoint = 'events';
+
+        if ($this->isBlank($description)) {
+            throw new Exception('Description is required');
+        }
+        if (strlen($description) > 150) {
+            throw new Exception('Description limited to 120 characters');
+        }
+        if (strlen($eventType) > 20) {
+            throw new Exception('Event Type limited to 20 characters');
+        }
+        if (strlen($eventURL) > 255) {
+            throw new Exception('Event URL limited to 255 characters');
+        }
+
+        $params = array('description'=>$description);
+        if (!$this->isBlank($createDate)) {
+            $params['createDate'] = $createDate;     
+        }
+        if (!$this->isBlank($eventURL)) {
+            $params['url'] = $eventURL;
+        }
+        if (!$this->isBlank($eventType)) {
+            $params['eventType'] = $eventType;
+        }
+        try {
+            return $this->execute_post_request($this->get_request_url($endpoint,null), $params);
+        } catch (Exception $e) {
+            throw new Exception('Unable to add event: ' . $e);
+        }  
+    }
+
+}
+?>
