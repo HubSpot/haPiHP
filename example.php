ef23afe6cda8d3b609f67fdd5bd6743c26d092a1
TheRealBenSmith <ben.adam.smith@gmail.com>
2011-11-22 21:58:54 -0500

    Updated tests and data
    Modified test data to use demo portal and added tests for marketing events api

--- example.php
+++ example.php
@@ -18,34 +18,55 @@
 */
 require_once 'class.leads.php';
 require_once 'class.settings.php';
+require_once 'class.events.php';
 
-$HAPIKey = 'some-really-long-api-key';
+$HAPIKey = 'demo';
 
 
-/*/Exercise Leads API
-$leads = new Leads($HAPIKey);
-$params = array('max'=>5, 'excludeConversionEvents'=>'true');
-print_r($base->get_leads($params));
+//Exercise Leads API
+    $leads = new Leads($HAPIKey);
+    
+    //Get list of leads
+    $params = array('max'=>5, 'excludeConversionEvents'=>'true');
+    print_r($leads->get_leads($params));
 
-$lead = $base->get_lead('8a706adf32896eda0132896fa1d3000b');
-$newvalues = array('firstName'=>'from','lastName'=>'haPiHP');
-echo $base->update_lead('8a706adf32896eda0132896fa1d3000b',$newvalues);        
-echo $base->close_lead('8a706adf32896eda0132896fa1d3000b',1234567890123);
-echo $lead->guid;
+    //Get specific lead
+    $lead = $leads->get_lead('8a41f2f22913e087012913e0b4b90011');
+    echo $lead->guid;
+    
+    //Update a lead 
+    $newvalues = array('firstName'=>'from','lastName'=>'haPiHP');
+    echo $leads->update_lead('8a41f2f22913e087012913e0b4b90011',$newvalues);        
 
-$postValues = array('email'=>'haPiHPtest@hubspot.com',
-                    'firstName'=>'Posted',
-                    'lastName'=>'By',
-                    'company'=>'haPiHP');
-//echo $base->add_lead('http://somedomain.hubspot.com/?app=leaddirector&FormName=contactUs', $postValues);
-*/
+    //Close a lead
+    echo $leads->close_lead('8a41f2f22913e087012913e0b4b90011',1234567890123);
+
+    //Add a lead
+    $postValues = array('email'=>'haPiHPtest@hubspot.com',
+                        'firstName'=>'Posted',
+                        'lastName'=>'By',
+                        'company'=>'haPiHP');
+    echo $leads->add_lead('http://demohubapi.app6.hubspot.com/?app=leaddirector&FormName=testform', $postValues);
+
 //Exercise Settings API
     $settings = new Settings($HAPIKey);
+
     //Get all settings
-    //print_r($settings->get_settings());
+    print_r($settings->get_settings());
+
     //Get a specific setting
-    print_r($settings->get_setting('BG:FollowMeEnabled'));
+    print_r($settings->get_setting('facebeezy'));
+
     //Update a setting
-    //echo $settings->update_setting('BenSmith','true');
+    echo $settings->update_setting('BenSmith','true');
     echo $settings->delete_setting('BenSmith');
+
+//Exercise Marketing Events API
+    $events = new Events($HAPIKey);
+
+    //Get all events
+    print_r($events->get_events());
+
+    //Add an event
+    echo $events->add_event('This is my test event',null,'http://www.test.com', 'test');
 ?>
